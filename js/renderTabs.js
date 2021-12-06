ac7a5f5a2b8329a78cbb588ec88469a6b4752fad
IgorBadyukov <badyukov1806302003@gmail.com>
2021-12-07 01:48:46 +0300

    рендер карточек

---
+++ js/renderTabs.js
@@ -0,0 +1,59 @@
+import getData from "./getData.js";
+
+const getCard = function (dataCard) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    switch (this) {
+        case 'video':
+            li.classList.add('card__video');
+            li.innerHTML = `
+            <a href="${dataCard.link}" class="card__link tube">
+             <figure class="card__video-set">
+                 <img src="${dataCard.preview}" alt="${dataCard.description}" class="card__video-img">
+                 <figcaption>${dataCard.description}</figcaption>
+             </figure>
+         </a>`;
+            break;
+        case 'photo':
+            li.classList.add('card__img');
+            li.innerHTML = `<img class="card__photo-img" src="${dataCard.link}" alt="${dataCard.description}">`;
+            break;
+        case 'goods':
+            li.classList.add('card__product');
+            li.innerHTML = `
+            <article class="product">
+            <img src="${dataCard.picture}" class="product__img" alt="${dataCard.name}">
+            <h2 class="product__title">${dataCard.name}</h2>
+            <div class="product__wrapper-buy">
+                <div class="product__price">${dataCard.price}₽</div>
+                <button class="product__btn-buy">
+                    <svg class="product__btn-icon">
+                        <use xlink:href="#add" />
+                    </svg>
+                </button>
+            </div>
+        </article>`;
+            break;
+        default:
+            li.innerHTML = 'Нет данных';
+            break;
+    }
+
+    return li;
+};
+
+const renderTabs = async (i = 0) => {
+    const tabsContent = document.querySelectorAll('.tabs__content');
+
+    const type = tabsContent[i].dataset.base;
+
+    const data = await getData(`db/${type}.json`);
+
+    const listElement = data.map(getCard, type);
+
+    tabsContent[i].textContent = '';
+    tabsContent[i].append(...listElement);
+};
+
+export default renderTabs;
