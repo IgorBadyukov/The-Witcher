e3d43cb58c772d56215f02ed9c204e0211019da0
IgorBadyukov <badyukov1806302003@gmail.com>
2021-12-07 01:52:43 +0300

    переключение табов

---
+++ js/tabs.js
@@ -0,0 +1,31 @@
+const tabs = option => {
+
+    const {
+        selectorTabsButton,
+        activeClassButton,
+        selectorTabsElement,
+        activeClassTab,
+        callback = () => {}
+    } = option;
+
+    const tabsBtns = document.querySelectorAll(selectorTabsButton);
+    const tabsElem = document.querySelectorAll(selectorTabsElement);
+
+    tabsBtns.forEach((btn) => {
+        btn.addEventListener('click', () =>{
+            for(let i = 0; i < tabsBtns.length; i++){
+                if(tabsBtns[i] === btn) {
+                    tabsBtns[i].classList.add(activeClassButton);
+                    tabsElem[i].classList.add(activeClassTab);
+                    callback(i);
+                }
+                else {
+                    tabsBtns[i].classList.remove(activeClassButton);
+                    tabsElem[i].classList.remove(activeClassTab);
+                }
+            }
+        });
+    });
+};
+
+export default tabs;
